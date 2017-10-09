0601c35de6d7a778ae8425069317bf43c2e9fae0
jmhunter <jh-github@ninja.org.uk>
2017-10-09 11:05:14 +0100

    Changes ready to send upstream (#1)
    * Add Arduino preferences file
    * Updated script to prompt graphically for refresh type. Also fixed local refresh
    * Show generic message when complete
    * Point directly at refresh script in git

--- brc.sh
+++ brc.sh
@@ -21,24 +21,46 @@ confirm () {
 
 confirmLocal () {
     # call with a prompt string or use a default
-    read -r -p "${1:-Are you sure? [y/N]} " response
-    case $response in
-        [lL]) 
-            true
-            ;;
-        *)
-            false
-            ;;
-    esac
+
+    if [ -z `which zenity` ]; then
+        # zenity is not installed - fall back to text mode
+        read -r -p "${1:-Are you sure? [y/N]} " response
+        case $response in
+            [lL]) 
+                true
+                ;;
+            *)
+                false
+                ;;
+        esac
+    else
+        # Prompt graphically
+        response=`zenity --list --title "${1:-Local or Remote Refresh}" \
+          --column="Type" --column="Description" \
+          local "Does not need Internet connectivity" \
+          remote "Pulls from online repository" \
+          2>/dev/null `
+        case $response in
+            local) 
+                true
+                ;;
+            remote)
+                false
+                ;;
+            *)
+                notify-send "Unknown response: '$response': exiting."
+		exit -1
+                ;;
+        esac
+    fi
 }
 
 
-if  ! confirm "Warning do you want to download and replace your build with v2?" ; then
-	exit
-fi
-
 (set -o igncr) 2>/dev/null && set -o igncr; # this comment is needed
 
+
+# Make sure we are in the home directory of the robot user, before we start
+cd ~
 
 if  confirmLocal "Local or Remote refresh ?" ; then
 	localRefresh=true
@@ -62,7 +84,7 @@ if [ $localRefresh == false ]; then
 	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 		ping_cmd='ping -q -c'
 		#echo "Linux"
-	elif [ "$(expr substr $(uname -s) 1 19)" == "CYGWIN_NT-6.2-WOW64" ]; then
+	elif [ "$(expr substr $(uname -s) 1 13)" == "CYGWIN_NT-6.2" ]; then
 		ping_cmd="$SYSTEMROOT/system32/ping -n"
 		#echo "Cygwin"
 	else
@@ -70,50 +92,42 @@ if [ $localRefresh == false ]; then
 	
 	fi
 
-	net_check_target="www.google.com"
+	net_check_target="github.com"
 	
 	connected=`$ping_cmd 1 $net_check_target &> /dev/null && echo 1 || echo 0`
 
 	if [ $connected == 0 ]; then
 		echo "Not connected, cannot refresh"
 		exit -1
+	else
+		echo "Yes, you are connected. Refreshing..."
 	fi
 	
-	# Do a clone and deal with it 
+	# Remove all old files; do a brand new clone and deal with it 
 	rm -rf RoboticsChallenge
 	git clone $MASTER_LOCATION
 
 else
 	echo "This will be a local refresh - all sketches will be deleted"
+
+	cd ~/RoboticsChallenge
+
+	# Remove all files (apart from perhaps .git, and anything similar that
+	# someone has been clever enough to create!)
+	rm -rf *
+
+	# Reset to latest GIT
+	git reset --hard
 fi
 
+# Empty the trash, if there are any files here
+rm -rf ~/.local/share/Trash/*
 
-
-# Remove everything from the master location locally
-
-echo "Removing all existing data... "
-rm -rf sketches
-rm -rf sketches.zip.*
-rm -rf sketches.zip
-rm -rf dhrobertson.com
-rm -f refresh_remote.sh*
-rm -f refresh_local.sh*
-rm -rf Desktop/Re*.desktop
-
-
-echo "Copying all folders to sketches"
-mkdir sketches
-find RoboticsChallenge   -maxdepth 1 -mindepth 1 -type d -exec cp -r {} sketches \;
-rm -rf sketches/.git
-rm -rf sketches/Demos
-rm -rf sketches/Projects
-rm -rf sketches/Testing
+# Reset Arduino Preferences
+cp -p ~/RoboticsChallenge/preferences.txt ~/.arduino15/
 
 # Copy the latest script down to replace this one running
-cp "RoboticsChallenge/BRC Refresh.desktop" ~/Desktop
+# (Could symlink it, but the symlink's icon is not aesthetically pleasing :) )
+cp -p "~/RoboticsChallenge/BRC Refresh.desktop" ~/Desktop
 
-
-echo "Fully Installed - restart Arduino or Processing to test"
-echo "Upgrading brc.sh - this will only work next time you run"
-cp RoboticsChallenge/brc.sh . 
-chmod +x brc.sh
+notify-send "Robot Refresh" "Completed refresh"
