950cf405ebde65364fb6c9e400d4b6d492a69731
Jonathan Hunter <jh-git@ninja.org.uk>
2018-10-09 13:59:07 +0100

    Fix typo
    Restore /boot into /boot, not into / ...

--- do_restore.sh
+++ do_restore.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo RSYNC_PASSWORD=roboteer325 rsync -avxPz --delete --exclude=lost+found/ robot-restore@robot01.ninja.org.uk::restore_master1_boot/ /
+sudo RSYNC_PASSWORD=roboteer325 rsync -avxPz --delete --exclude=lost+found/ robot-restore@robot01.ninja.org.uk::restore_master1_boot/ /boot/
 sudo RSYNC_PASSWORD=roboteer325 rsync -avxPz --delete --exclude=lost+found/ robot-restore@robot01.ninja.org.uk::restore_master1_/ /
