c2e622357369c1b395a23146ab7dae42f11a0953
Jonathan Hunter <jh-git@ninja.org.uk>
2018-10-11 12:27:44 +0100

    Add some basic error handling, and progress updates

--- do_backup.sh
+++ do_backup.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
-sudo rsync -avxPz --delete --exclude=lost+found/ /boot/ robot-update@robot01.ninja.org.uk::backup_master1_boot/
-sudo rsync -avxPz --delete --exclude=lost+found/ / robot-update@robot01.ninja.org.uk::backup_master1_/
+cat <<__EOF__
+
+About to update master image (hosted online) from local laptop
+
+You will need to enter the local password for the robot user, when prompted
+by sudo below - and then also the remote password for the master image, when
+prompted by rsync.
+
+__EOF__
+
+sudo rsync -avxPz --delete \
+	--exclude=lost+found/ \
+	/boot/ \
+	robot-update@robot01.ninja.org.uk::restore_master1_boot/  && \
+sudo rsync -avxPz --delete \
+	--exclude=lost+found/ \
+	/ \
+	robot-update@robot01.ninja.org.uk::restore_master1_/ \
+
+if [ $? == 0 ]; then
+	read -n -p "Successfully backed up. Press ENTER to close"
+else
+	read -n -p "Failed to back up due to one or more errors. Press ENTER to close, then please try again"
+fi
--- do_restore.sh
+++ do_restore.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
-sudo RSYNC_PASSWORD=roboteer325 rsync -avxPz --delete --exclude=lost+found/ robot-restore@robot01.ninja.org.uk::restore_master1_boot/ /boot/
-sudo RSYNC_PASSWORD=roboteer325 rsync -avxPz --delete --exclude=lost+found/ robot-restore@robot01.ninja.org.uk::restore_master1_/ /
+cat <<__EOF__
+
+About to restore entire laptop from master image (hosted online)
+
+You will need to enter the local password for the robot user, when prompted
+by sudo below.
+
+__EOF__
+
+sudo RSYNC_PASSWORD=roboteer325 \
+	rsync -avxPz --delete \
+	--exclude=lost+found/ \
+	robot-restore@robot01.ninja.org.uk::restore_master1_boot/ \
+	/boot/ && \
+sudo RSYNC_PASSWORD=roboteer325 \
+	rsync -avxPz --delete \
+	--exclude=lost+found/ \
+	robot-restore@robot01.ninja.org.uk::restore_master1_/ \
+	/
+
+if [ $? == 0 ]; then
+	echo Successfully restored - rebooting now
+	sudo shutdown -r now
+else
+	read -n -p "Failed to restore due to one or more errors. Press ENTER to close, then please try again"
+fi
